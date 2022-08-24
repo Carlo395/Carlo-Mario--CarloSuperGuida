8c5658529d46559e7e80a5542259b8b3461531d5
Carlo395 <carlomario1@outlook.it>
2022-08-24 14:26:16 +0200

    add page

---
+++ main.js
@@ -0,0 +1,21 @@
+/* Sending Email from Contact Section */
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_hgkk8ab';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
